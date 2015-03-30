a183ac2b2e95cea931a530cf3cdf0c5a60048e2b
DexKai <dex.pgsoto@gmail.com>
2015-03-30 02:29:50 -0300

    Conexión Base Datos MySQL
    Cambio nombre sistema y compañia

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=TISW_GRUPO2',
+    'username' => 'TISW_GRUPO2',
+    'password' => 'gimnasio2015',
     'charset' => 'utf8',
 ];
