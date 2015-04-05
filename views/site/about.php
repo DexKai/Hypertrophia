2b033ecaf3ca89633e115431c4191433da49e110
emanuelsebastian <eminostr@alumnos.ubiobio.cl>
2015-04-05 13:22:13 -0400

    Actualizar Quienes Somos
    Se agrego mision, vision y otros más

--- views/site/about.php
+++ views/site/about.php
@@ -2,15 +2,60 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-$this->title = 'About';
+$this->title = '¿Quienes Somos?';
 $this->params['breadcrumbs'][] = $this->title;
-?>
-<div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
+?><h1>Nosotros Somos</h1>
+<div class="container-fluid">
 
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
+	<h3>Historia:</h3>
+	
+	
+	<div><p>Hace veinte años soñamos con hacer un cambio positivo en la calidad de vida de los chilenos.
 
-    <code><?= __FILE__ ?></code>
+Hoy sentimos que hemos cumplido nuestra misión y queremos compartirlo contigo. </p>
+
+<p>Estamos convencidos que existe un gran potencial de crecimiento para GymFitness, mientras mantengamos las premisas de proveer un producto de alta calidad y soluciones integrales a nuestros alumnos.</p>
+
+<p>Nuestro desafío es seguir avanzando en este sueño con la misma pasión y los valores que nos inspiran día a día a mejorar la salud y bienestar de nuestra gente.</p> 
+
+<p>GymFitness, te cambia la vida</p></div>
+	
+	<h3>Mision:</h3>
+	<div>  
+
+Somos una empresa dedicada a la administración y operación de centros deportivos, con el propósito de educar y motivar a todas las personas a mejorar su calidad de vida, a través de una experiencia única e integral.
+
+Nuestra Misión es Educar y Motivar a todas las personas a mejorar su bienestar, entregando una experiencia única con un servicio de excelencia a través de una cultura deportiva integral.
+</div>
+	<br>
+	<h3>Vision:</h3>
+	<div>Ser la cadena de gimnasios líder en participación de mercado y cobertura nacional siendo la mejor opción para nuestros clientes y empleados, que disfrutan de su actividad física y privilegia el desarrollo personal y profesional de sus integrantes.
+Ser la mejor opción en la industria del Fitness en el mercado chileno para nuestros clientes y empleados, líder en participación de mercado y en cobertura a nivel nacional, por medio de proveer servicios de excelencia acordes a las necesidades del consumidor, entregando una “experiencia” única a nuestros socios.
+
+Desarrollar una organización que disfruta con lo que hace, donde se privilegia el desarrollo personal y profesional de sus empleados, centrada en el servicio al cliente, enfocada en el desarrollo de la marca y su posicionamiento en el mercado.</div>
+
+<h3>Valores:</h3>
+<div>
+<strong>Pasión</strong>
+<p>Somos apasionados por nuestro trabajo, anteponemos esta pasión por encima de todas las dificultades y nos entusiasman los desafíos que nos prepara el futuro.
+</p>
+
+<strong>Colaboración</strong>
+<p>
+Buscamos el bienestar colectivo, con mentalidad de equipo que facilite la superación de las debilidades y potencie nuestras fortalezas.
+</p>
+<strong>Calidad</strong>
+
+<p>Realizamos nuestro trabajo con excelencia, buscando siempre la perfección en nuestras acciones y el mejoramiento continuo.</p>
+
+<strong>Integridad</strong>
+
+<p>Actuamos con honradez, enfrentamos nuestro trabajo con sinceridad y ética.</p>
+
+<strong>Respeto</strong>
+
+<p>Promovemos relaciones humanas cordiales, respetuosas y armoniosas con nuestros clientes y comunidad, respetamos la diversidad y reconocemos el diálogo como herramienta esencial para la solución de conflictos y diferencias.
+</p>
+</div>
+
 </div>
