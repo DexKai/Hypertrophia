e28a41d6112e7893d1d8a2087db78a65c486dc7b
ravenkroft <jsalazarllanos@gmail.com>
2015-04-10 11:06:32 -0400

    Cambio Usuario Base Datos

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=TISW_GRUPO2',
-    'username' => 'TISW_GRUPO2',
-    'password' => 'gimnasio2015',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 ];
