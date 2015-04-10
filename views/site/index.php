e4c87e8945926109b1526529b464779d3f681e03
emanuelsebastian <eminostr@alumnos.ubiobio.cl>
2015-04-10 10:51:31 -0300

    Actualizacion de index
    actualizar index

--- views/site/index.php
+++ views/site/index.php
@@ -2,24 +2,70 @@
 /* @var $this yii\web\View */
 $this->title = 'My Yii Application';
 ?>
+
+<div class="jumbotron">
+
+    <hr>
+<div class='col-lg-offset-6 col-lg-6 carousel slide' data-ride='carousel' id='image-slider'> 
+<ol class='carousel-indicators'>
+        <li data-target='#image-slider' data-slide-to='0' class='active'></li>
+        <li data-target='#image-slider' data-slide-to='1' </li>
+        <li data-target='#image-slider' data-slide-to='2' </li>
+
+</ol>
+        <div class="carouel-inner">
+            <div class='item active'>
+                <img src='images/slide-1.jpg' alt='imagen-1' />
+            <div class='carousel-caption'>
+            <h1> Caption title</h1>
+            <p>Hola :D</p>
+            </div>
+            
+            <div class='item'>
+                <img src='images/slide-2.jpg' alt='imagen-2' />
+            </div>
+            
+            <div class='item'>
+                <img src='images/slide-3.jpg' alt='imagen-3' />
+            </div>
+        
+        <a href='#image-slider' class='left carousel-control' data-slider='prev'>
+            <span class='glyphicon glyphicon-chevron-left'></span>
+        </a>
+        <a href='#image-slider' class='right carousel-control' data-slider='next'>
+            <span class='glyphicon glyphicon-chevron-right'></span>
+        </a>
+
+        
+    
+    </div>
+
+<hr>
+
+
+
+
 <div class="site-index">
 
     <div class="jumbotron">
+        
         <h1>Congratulations!</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    
+
+
     </div>
 
+
+
+</div>
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-
-
-
-
 
 
                 <h2>Heading</h2>
