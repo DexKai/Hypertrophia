1f76d2eb0be82559264991baa2910b0181732f81
ravenkroft <jsalazarllanos@gmail.com>
2015-04-15 17:12:35 -0400

    Actualización profesor

--- config/db.php
+++ config/db.php
@@ -2,12 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-<<<<<<< HEAD
+    'dsn' => 'mysql:host=localhost;dbname=desarrollo',
     'dsn' => 'mysql:host=localhost;dbname=TISW_GRUPO2',
-=======
-    'dsn' => 'mysql:host=localhost;dbname=tisw_grupo2',
->>>>>>> origin/master
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
-];
+    ];
