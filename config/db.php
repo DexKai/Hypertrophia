51034d0c33522b0991e4a71ac5c2a8c0f8129b35
DexKai <dex.pgsoto@gmail.com>
2015-04-15 00:30:10 -0300

    Actualizacion config bd

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=TISW_GRUPO2',
-    'username' => 'TISW_GRUPO2',
-    'password' => 'gimnasio2015',
+    'dsn' => 'mysql:host=localhost;dbname=tisw_grupo2',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 ];
