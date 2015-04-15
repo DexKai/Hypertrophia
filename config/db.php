21a27d845afe24515a9693b46816299da0102e70
emanuelsebastian <eminostr@alumnos.ubiobio.cl>
2015-04-15 01:10:48 -0300

    Modificar nombre de basedato

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=desarrollo',
+    'dsn' => 'mysql:host=localhost;dbname=TISW_GRUPO2',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
