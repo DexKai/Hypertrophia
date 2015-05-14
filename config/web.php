b74aafe001b705fd2dfb452102f8c58b650fd327
DexKai <dex.pgsoto@gmail.com>
2015-05-14 15:29:27 -0300

    oculta id

--- config/web.php
+++ config/web.php
@@ -45,12 +45,17 @@ $config = [
             ],
         ],
         'urlManager' => [
-            'enablePrettyUrl' => true,
+            'class' => 'yii\web\UrlManager',
+            // Disable index.php
             'showScriptName' => false,
+            // Disable r= routes
+            'enablePrettyUrl' => true,
             'enableStrictParsing' => false,
             'suffix' => '.html',
             'rules' => array(
-                    //.....
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
             ),
 
         ],
