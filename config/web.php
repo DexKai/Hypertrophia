245e0631f4294671e025fd26aec8f395ca27331f
DexKai <dex.pgsoto@gmail.com>
2015-05-15 05:07:53 -0300

    desabilitada barra debug, reduccion vista usuario

--- config/web.php
+++ config/web.php
@@ -88,7 +88,7 @@ $config = [
     ],
     'params' => $params,
 ];
-
+/*
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
@@ -97,5 +97,5 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = 'yii\gii\Module';
 }
-
+*/
 return $config;
