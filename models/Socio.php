ae72fb74077bf1d778833bfa7a4b738d62e50014
Yojans Cid <ycid@alumnos.ubiobio.cl>
2015-07-02 01:25:08 -0300

    validacion rut socio (otra vez)

--- models/Socio.php
+++ models/Socio.php
@@ -36,17 +36,29 @@ class Socio extends \yii\db\ActiveRecord
     {
         return [
             [['PROG_id', 'IM_id', 'PA_id'], 'integer'],
+
             [['SO_nombre', 'SO_apellido_materno', 'SO_apellido_paterno', 'SO_direccion', 'SO_estado_actividad'], 'string', 'max' => 20],
+          
           [['SO_rut'], 'validarRut'],
+          
             [['SO_rut'], 'unique', 'message'=>'Rut ya existe'],
+             [['SO_rut'],'validarSoloNumerosYGuion'],
           [['SO_email'], 'email'],
+          
             [['SO_email'], 'unique', 'message'=>'correo ya existe'],
+          
           [['SO_nombre'], 'required', 'message'=>'compo requerido'],
+          
           [['SO_apellido_materno'], 'required', 'message'=>'compo requerido'],
+          
           [['SO_apellido_paterno'], 'required', 'message'=>'compo requerido'],
+          
           [['SO_direccion'], 'required', 'message'=>'compo requerido'],
+          
           [['SO_rut'], 'required', 'message'=>'compo requerido'],
+          
           [['SO_email'], 'required', 'message'=>'compo requerido'],
+          
           [['SO_estado_actividad'], 'required', 'message'=>'compo requerido'],
 
           [['SO_nombre'], 'match',"pattern" => '/^[a-zA-Z ñÑáéíóúüç]*$/', 'message'=>'Solo se pueden utilizar letras'],
@@ -76,6 +88,21 @@ class Socio extends \yii\db\ActiveRecord
         ];
     }
 
+
+
+
+
+ public function validarSoloNumerosYGuion($attribute,$params)
+        {
+        
+          $pattern ='/(?!^[0-9]*$)(-)/';
+
+         
+        if(!preg_match($pattern, $this->$attribute))
+          $this->addError($attribute, 
+          'Rut solo debe contener digitos y un guion');
+          
+        }
 
 
 
